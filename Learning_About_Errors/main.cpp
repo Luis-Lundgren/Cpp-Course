741677b800cbefb117cf92a579116430a9c271bb
Luis Lundgren <126507747+Luis-Lundgren@users.noreply.github.com>
2023-02-28 15:38:58 -0800

    Add files via upload

---
+++ Learning_About_Errors/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+
+    //Compile time error remove the ;
+    std::cout << "Hello World in C++20" << std::endl;
+
+    //Run time error
+    // int value = 7/0;
+    // std::cout << "value : " << value << std::endl;
+    return 0;
+}
